add3d1892d5d125073abefedd1d91019fe81ef69
Charmaine Halim <89816977+c1217h03@users.noreply.github.com>
2024-06-04 18:02:09 +0700

    Finished functionality of start button.
    Maximum grid width is 50. And when user specifies another width and height, the current grid is removed.

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,41 @@
 let container = document.querySelector("div#container");
+container.addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "grey";
+});
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let square = document.createElement("div");
-    square.id = "square";
-    container.appendChild(square);
+let start_btn = document.querySelector("#start");
+start_btn.addEventListener("click", () => {
+  let num_grid = prompt("Enter the width and height for the grid: ");
+
+  if (container.firstChild) {
+    clearGrid();
+  }
+
+  while (num_grid > 50) {
+    alert("You have exceeded the grid size. Pick another number");
+    num_grid = prompt("Enter the width and height for the grid: ");
+  }
+
+  container.style.maxWidth = num_grid * 15 + "px";
+
+  for (let i = 0; i < num_grid; i++) {
+    for (let j = 0; j < num_grid; j++) {
+      let square = document.createElement("div");
+      square.id = "square";
+      container.appendChild(square);
+    }
+  }
+});
+
+function clearGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
 }
 
-let square = document.querySelectorAll("#square");
-container.addEventListener("mouseover", (event) => {
-  event.target.style.backgroundColor = "orange";
-});
+function resetGrid() {
+  let squares = container.querySelectorAll("#square");
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = "white";
+  }
+}
